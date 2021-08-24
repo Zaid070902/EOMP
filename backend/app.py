431dba89fe8bd11285bb297b58e3786489554758
zaid070902 <zaidflandorp4@gmail.com>
2021-08-24 16:26:44 +0200

    update

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ import sqlite3
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_mail import Mail, Message
+import cloudinary
+import cloudinary.uploader
 
 
 class Users(object):
@@ -15,10 +17,13 @@ class Users(object):
 
 
 class Beat(object):
-    def __init__(self, beat_name, beat_type, beat_tempo):
+    def __init__(self, id, beat_name, beat_type, beat_tempo, image, producer):
+        self.id = id
         self.beat_name = beat_name
         self.beat_type = beat_type
         self.beat_tempo = beat_tempo
+        self.image = image
+        self.producer = producer
 
 
 def usertable():
@@ -28,7 +33,7 @@ def usertable():
     conn.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "username TEXT NOT NULL,"
                  "password TEXT NOT NULL,"
-                 "email TEXT NOT NULL,")
+                 "email TEXT NOT NULL)")
     print("Table created")
     conn.close()
 
@@ -54,9 +59,12 @@ user = get_users()
 
 def beats_table():
     with sqlite3.connect('Store.db') as conn:
-        conn.execute("CREATE TABLE IS NOT EXISTS beats (beat_name TEXT NOT NULL,"
+        conn.execute("CREATE TABLE IF NOT EXISTS beats (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                     "beat_name TEXT NOT NULL,"
                      "beat_type TEXT NOT NULL,"
-                     "beat_tempo TEXT NOT NULL,")
+                     "beat_tempo TEXT NOT NULL,"
+                     "image TEXT NOT NULL,"
+                     "producer TEXT NOT NULL)")
         print("beat table created")
 
 
@@ -67,18 +75,33 @@ def get_beats():
     with sqlite3.connect('Store.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM beats")
-        beats = cursor.fetchall()
+        beat = cursor.fetchall()
 
         beat_data = []
 
-        for data in beats:
-            beat_data.append(Beat(data[0], data[1], data[2]))
+        for data in beat:
+            beat_data.append(Beat(data[0], data[1], data[2], data[3], data[4], data[5]))
     return beat_data
 
 
-beats = beats_table()
+beats_table()
 
-# username_table = {u.username: u for u in user}
+
+def image_convert():
+    app.logger.info('in upload route')
+    cloudinary.config(cloud_name="dh7g4uw8x",
+                      api_key="848147445884154",
+                      api_secret="HwbSS8r41xhx6tYhQ_KC7TulLL4")
+    upload_result = None
+    if request.method == 'POST' or request.method == 'PUT':
+        beat_img = request.files['image']
+        app.logger.info('%s file_to_upload', beat_img)
+        if beat_img:
+            upload_result = cloudinary.uploader.upload(beat_img)
+            app.logger.info(upload_result)
+            return upload_result['url']
+
+    # username_table = {u.username: u for u in user}
 # userid_table = {u.id: u for u in user}
 #
 #
@@ -130,3 +153,124 @@ def registration():
         mail.send(msg)
         return "Message send"
     return response
+
+
+@app.route('/create-beat/', methods=["POST"])
+def create_beat():
+    response = {}
+
+    if request.method == "POST":
+        beat_name = request.form['beat_name']
+        beat_type = request.form['beat_type']
+        beat_tempo = request.form['beat_tempo']
+        beat_img = image_convert()
+        producer = request.form['producer']
+
+        with sqlite3.connect('Store.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO beats("
+                           "id"
+                           "beat_name,"
+                           "beat_type,"
+                           "beat_tempo,"
+                           "image,"
+                           "producer) VALUES(?, ?, ?, ?, ?, ?)", (id, beat_name, beat_type, beat_tempo, beat_img, producer))
+            conn.commit()
+            response["message"] = "success"
+            response["status_code"] = 201
+        return response
+
+
+@app.route('/display-users/', methods=["GET"])
+def get_users():
+    response = {}
+    with sqlite3.connect("Store.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+
+        posts = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = posts
+    return response
+
+
+@app.route('/display-beats/', methods=["GET"])
+def get_blogs():
+    response = {}
+    with sqlite3.connect("Store.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM beats")
+
+        posts = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = posts
+    return jsonify(response)
+
+
+@app.route("/delete-beat/<int:id>")
+def delete_post(id):
+    response = {}
+    with sqlite3.connect("Store.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM beats WHERE id=" + str(id))
+        conn.commit()
+        response['status_code'] = 200
+        response['message'] = "beat deleted successfully."
+        return response
+
+
+@app.route('/edit-post/<int:id>/', methods=["PUT"])
+def edit_post(id):
+    response = {}
+
+    if request.method == "PUT":
+        with sqlite3.connect('Store.db') as conn:
+            beat_name = request.form['beat_name']
+            beat_type = request.form['beat_type']
+            beat_tempo = request.form['beat_tempo']
+            beat_img = request.files['beat_img']
+            producer = request.form['producer']
+            put_data = {}
+
+            if beat_name is not None:
+                put_data["beat_name"] = beat_name
+                cursor = conn.cursor()
+                cursor.execute("UPDATE beats SET beat_name =? WHERE id=?", (put_data["beat_name"], id))
+                conn.commit()
+                response['message'] = "Update was successfully"
+                response['status_code'] = 200
+            if beat_type is not None:
+                put_data['beat_type'] = beat_type
+                cursor = conn.cursor()
+                cursor.execute("UPDATE beats SET beat_type =? WHERE id=?", (put_data["beat_type"], id))
+                conn.commit()
+                response["beat_type"] = "Content updated successfully"
+                response["status_code"] = 200
+            if beat_tempo is not None:
+                put_data['beat_tempo'] = beat_tempo
+                cursor = conn.cursor()
+                cursor.execute("UPDATE beats SET beat_tempo =? WHERE id=?", (put_data["beat_tempo"], id))
+                conn.commit()
+                response["beat_tempo"] = "Content updated successfully"
+                response["status_code"] = 200
+            if beat_img is not None:
+                put_data['beat_img'] = image_convert()
+                cursor = conn.cursor()
+                cursor.execute("UPDATE beats SET beat_img =? WHERE id=?", (put_data["beat_img"], id))
+                conn.commit()
+                response["beat_img"] = "Content updated successfully"
+                response["status_code"] = 200
+            if producer is not None:
+                put_data['producer'] = producer
+                cursor = conn.cursor()
+                cursor.execute("UPDATE beats SET producer =? WHERE id=?", (put_data["producer"], id))
+                conn.commit()
+                response["producer"] = "Content updated successfully"
+                response["status_code"] = 200
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
